863b3c12dbc459e1e1774a075af352ad296a5ded
jakaria08 <jakaria.rabbi@live.com>
2018-03-27 19:53:34 -0600

    Background Service for IBM

---
+++ app/src/main/java/com/rabbi/jakaria/project_iot/Service_Analyze_IBM.java
@@ -0,0 +1,16 @@
+package com.rabbi.jakaria.project_iot;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class Service_Analyze_IBM extends Service {
+    public Service_Analyze_IBM() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
---
+++ app/src/main/java/com/rabbi/jakaria/project_iot/Service_Analyze_Indico.java
@@ -0,0 +1,16 @@
+package com.rabbi.jakaria.project_iot;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class Service_Analyze_Indico extends Service {
+    public Service_Analyze_Indico() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
