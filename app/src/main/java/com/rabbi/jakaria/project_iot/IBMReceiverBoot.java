8ab43745ac79dfb9fda28077324a50c8e994e0c3
jakaria08 <jakaria.rabbi@live.com>
2018-04-01 16:28:06 -0600

    Background service nearly complet, boot up is not working

--- app/src/main/java/com/rabbi/jakaria/project_iot/IBMReceiverBoot.java
+++ app/src/main/java/com/rabbi/jakaria/project_iot/IBMReceiverBoot.java
@@ -9,12 +9,12 @@ import android.util.Log;
  * Created by root on 31/03/18.
  */
 
-public class IBMReceiver_BOOT extends BroadcastReceiver {
+public class IBMReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context rcontext, Intent intent)
     {
-        Log.e("Broadcast", "inside on receive of IBMReceiver_BOOT");
+        Log.e("Broadcast", "inside on receive of IBMReceiver");
         Intent service1 = new Intent(rcontext, Service_Analyze_IBM.class);
         rcontext.startService(service1);
 
--- app/src/main/java/com/rabbi/jakaria/project_iot/IndicoReceiverBoot.java
+++ app/src/main/java/com/rabbi/jakaria/project_iot/IndicoReceiverBoot.java
@@ -9,12 +9,12 @@ import android.util.Log;
  * Created by root on 31/03/18.
  */
 
-public class IndicoReceiver_BOOT extends BroadcastReceiver {
+public class IndicoReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context rcontext, Intent intent)
     {
-        Log.e("Broadcast", "inside on receive of IndicoReceiver_BOOT");
+        Log.e("Broadcast", "inside on receive of IndicoReceiver");
         Intent service1 = new Intent(rcontext, Service_Analyze_Indico.class);
         rcontext.startService(service1);
 
